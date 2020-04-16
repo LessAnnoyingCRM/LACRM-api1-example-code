fa1bb16c17a651479086ae6580122da186fb8a3a
Tyler King <tilikang@users.noreply.github.com>
2020-04-16 13:02:13 -0500

    Update get_pipeline_report.php

--- examples/get_pipeline_report.php
+++ examples/get_pipeline_report.php
@@ -1,6 +1,6 @@
 <?php
 
-include('../init.php');
+include('init.php');
 
 $Function = "GetPipelineReport";
 
@@ -12,3 +12,4 @@ $Result = CallAPI($Function, $Parameters);
  
 DebugOutput($Result);
  
+?>
