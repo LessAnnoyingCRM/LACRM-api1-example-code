2e9cfb44c3578ea9863739ce3f8d5ced2f811a4f
Tyler King <tilikang@users.noreply.github.com>
2020-04-16 13:01:06 -0500

    Update create_pipeline.php

--- examples/create_pipeline.php
+++ examples/create_pipeline.php
@@ -1,6 +1,6 @@
 <?php
 
-include('../init.php');
+include('init.php');
 
 $Function = "CreatePipeline";
 
@@ -15,3 +15,4 @@ $Result = CallAPI($Function, $Parameters);
  
 DebugOutput($Result);
  
+?>
