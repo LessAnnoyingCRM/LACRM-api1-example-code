5f8aeba746384658fabab176858400a937b48ef2
Tyler King <tilikang@users.noreply.github.com>
2020-04-23 14:25:17 -0500

    Update search_contacts.php

--- examples/search_contacts.php
+++ examples/search_contacts.php
@@ -5,7 +5,7 @@ include('init.php');
 $Function = "SearchContacts";
 
 $Parameters = array(
-	'SearchTerms'=>"Jack Box", //paste the ContactId you got from create_contact.php
+	'SearchTerms'=>"", //paste the ContactId you got from create_contact.php
 );
 
 $Result = CallAPI($Function, $Parameters);
